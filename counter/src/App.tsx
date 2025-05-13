f6556528722c2d386bc99ebbff7146004f4299d7
Shravan250 <shravanbobade25@gmail.com>
2025-05-13 10:12:37 +0530

    implemented counter

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="container">
+          <h1>Counter</h1>
+          <span
+            id="value"
+            style={{
+              color: count > 0 ? "green" : count === 0 ? "#222" : "red",
+            }}
+          >
+            {count}
+          </span>
+          <div className="button-container">
+            <button
+              className="btn decrease"
+              onClick={() => setCount(count - 1)}
+            >
+              decrease
+            </button>
+            <button className="btn reset" onClick={() => setCount(0)}>
+              reset
+            </button>
+            <button
+              className="btn increase"
+              onClick={() => setCount(count + 1)}
+            >
+              increase
+            </button>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
